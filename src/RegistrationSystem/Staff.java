9feded58f19e7d91db1a79b8bf767da2e348e3fb
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2022-11-22 10:58:27 -0500

    staff constructors

--- src/RegistrationSystem/Staff.java
+++ src/RegistrationSystem/Staff.java
@@ -13,7 +13,14 @@ public class Staff extends SwimPrograms{
     private String instructor;
     private String supervisor;
     
+    public Staff(String address, String poolName, String features, int area, boolean publicSwim, boolean privateSwim, String lifeguard, String instructor, String supervisor){
+        // Call to super constructor
+        super(address, poolName, features, area, publicSwim, privateSwim); 
 
+        this.lifeguard = lifeguard;
+        this.instructor = instructor;
+        this.supervisor = supervisor;
+    }
     
 
 
