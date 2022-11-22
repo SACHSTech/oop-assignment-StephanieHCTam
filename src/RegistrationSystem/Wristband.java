ec2c6856e9feaa04500402f21b7cacb4be5dc95f
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2022-11-22 17:32:07 -0500

    wristband constructor

--- src/RegistrationSystem/Wristband.java
+++ src/RegistrationSystem/Wristband.java
@@ -1,4 +1,17 @@
 package RegistrationSystem;
-public class Wristband {
+public class Wristband extends Patron{
+
+    private String red;
+    private String yellow;
+    private String green;
     
+    public Wristband(String address, String poolName, String features, int area, boolean publicSwim, boolean privateSwim, String name, int age, String email, String red, String yellow, String green){
+        // Call to super constructor
+        super(address, poolName, features, area, publicSwim, privateSwim, name, age, email); 
+
+        this.red = red;
+        this.yellow = yellow;
+        this.green = green;
+
+    }
 }
