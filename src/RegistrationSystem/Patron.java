3a624545caa62e6faffc250038fe4246c88b62ff
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2022-11-22 01:47:32 -0500

    Patron class file

--- src/RegistrationSystem/Patron.java
+++ src/RegistrationSystem/Patron.java
@@ -1,4 +1,52 @@
 package RegistrationSystem;
+
+/**
+ * Patron class file, extends SwimPrograms
+ * @author S. Tam
+ * 
+ */
+
 public class Patron extends SwimPrograms {
-    
+
+    // instance variables
+    private String name;
+    private int age;
+    private String email;
+
+    /**
+     * Constructor: creates new instance for a patron registering a swim program
+     * @param name patron name
+     * @param age patron age
+     * @param email patron's email
+     */
+    public Patron(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;  
+    }
+
+    /**
+     * Returns patron's name
+     * @return String address
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns patron's age
+     * @return int age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Returns patron's email
+     * @return String email
+     */
+    public String getEmail() {
+        return email;
+    }
 }
