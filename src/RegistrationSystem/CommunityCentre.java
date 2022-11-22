8e6b224cf9d17398d100aa17ad475d1263cda02f
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2022-11-22 10:31:22 -0500

    Output Community Center pool info

--- src/RegistrationSystem/CommunityCentre.java
+++ src/RegistrationSystem/CommunityCentre.java
@@ -62,8 +62,8 @@ public class CommunityCentre {
     }
 
     public String toString(){
-        return ("Pool: " + poolName);
-    }
+        return ("Pool: " + poolName + ", " + address + ", " + features + ", " + area);
 
+    }
     
 }
